9958393cd32c6267f9ae5414ffb20edcb887405c
ndegel <nik.32@web.de>
2020-12-13 15:37:53 +0100

    Update update.bat
    Schreibfehler bei ehrenwort.tex (vorher: eherenwort.tex), hatte bei mir die Datei eherenwort.tex mit dem Inhalt "404: Not Found" erzeugt

--- update.bat
+++ update.bat
@@ -5,7 +5,7 @@ curl %GIT_DHGE_LATEX_HOME%/template.tex -o template.tex
 mkdir components
 curl %GIT_DHGE_LATEX_HOME%/components/com.tex -o components/com.tex
 curl %GIT_DHGE_LATEX_HOME%/components/deckblatt.tex -o components/deckblatt.tex
-curl %GIT_DHGE_LATEX_HOME%/components/eherenwort.tex -o components/eherenwort.tex
+curl %GIT_DHGE_LATEX_HOME%/components/ehrenwort.tex -o components/ehrenwort.tex
 curl %GIT_DHGE_LATEX_HOME%/components/package.config.tex -o components/package.config.tex
 curl %GIT_DHGE_LATEX_HOME%/components/setup.tex -o components/setup.tex
 curl %GIT_DHGE_LATEX_HOME%/components/alphabetic-ibid.cbx -o components/alphabetic-ibid.cbx
